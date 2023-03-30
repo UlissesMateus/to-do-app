b495d39405032303d3e9a1341779286be9814770
UlissesMateus <ulissesmateus01@gmail.com>
2023-03-29 21:07:27 -0300

    Inicializando Projeto

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import { Text } from '../Text';
+import { Container } from './styles';
+
+export default function Header(){
+    return (
+        <Container>
+          <Text>Ola</Text>  
+        </Container>
+    );
+}
