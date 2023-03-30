9b6bd40264a697240327dc4a44c5c709a91aaddc
UlissesMateus <ulissesmateus01@gmail.com>
2023-03-29 21:07:57 -0300

    inicializando

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,11 @@ import { Container } from './styles';
 export default function Header(){
     return (
         <Container>
-          <Text>Ola</Text>  
+            <Text size={24}>
+            ToDO
+            <Text size={24} weight={"700"}>APP</Text>
+            </Text>
+            <Text opacity={0.5}>Add, delete ou marque as tarefas executadas</Text>
         </Container>
     );
 }
